52967f03f0d86aa79a79c3c39b0d8927b7250af2
ccpezeshki <54556598+ccpezeshki@users.noreply.github.com>
2019-09-04 13:22:23 -0700

    Add files via upload

--- Prob3.py
+++ Prob3.py
@@ -1,31 +1,30 @@
-#---------------- Problem 3 ------------------
-#
-# Name:
-#
-# Who did you work with (if anyone)?:
-#
-# --------------------------------------------
+numx=5
+numy=9
+numz=7
 
-
-def big_odd_finder(): # Pls don't change this line, for my own purposes
-    # Your code goes below here and WILL NEED TO BE INDENTED like
-    # these comments are (sorry, it has to do with my own purpose above)
-
-    # Prompt for variable inputs
-
-
-
-    # Logic to check for being largest odd:
-
-
-
-
-
-    # Print out largest odd value
-    # or "No odd numbers found." if no odds exist
-
-
-
-# Don't modify code below here please!
-if __name__ == '__main__':
-    big_odd_finder()
+if not numx%2==0 and numy%2==0 and numz%2==0:
+    print(numx)
+if numx%2==0 and not numy%2==0 and numz%2==0:
+    print(numy)
+if numx%2==0 and numy%2==0 and not numz%2==0:
+    print(numz)
+if not numx%2==0 and not numy%2==0 and numz%2==0 and numx>numy:
+    print(numx)
+if not numx%2==0 and numy%2==0 and not numz%2==0 and numx>numy:
+    print(numx)
+if not numx%2==0 and not numy%2==0 and numz%2==0 and numy>numx:
+    print(numy)
+if numx%2==0 and not numy%2==0 and not numz%2==0 and numy>numz:
+    print(numy)
+if not numx%2==0 and numy%2==0 and not numz%2==0 and numz>numx:
+    print(numz)
+if numx%2==0 and not numy%2==0 and not numz%2==0 and numz>numy:
+    print(numz)
+if not numx%2==0 and not numy%2==0 and not numy%2==0 and numx>numy and numx>numz:
+    print(numx)
+if not numx%2==0 and not numy%2==0 and not numy%2==0 and numy>numx and numy>numz:
+    print(numy)
+if not numx%2==0 and not numy%2==0 and not numy%2==0 and numz>numx and numz>numy:
+    print(numz)
+if numx%2==0 and numy%2==0 and numz%2==0:
+    print("No odd numbers found")
--- Prob4.py
+++ Prob4.py
@@ -1,40 +1,13 @@
-#---------------- Problem 4 ------------------
-#
-# Name:
-#
-# Who did you work with (if anyone)?:
-#
-# --------------------------------------------
+A, B, d = 1,2,3 
 
-def find_area(A,B,d): # Don't change this line, it is for my own purposes
-    # Your code goes below here and WILL NEED TO BE INDENTED like
-    # these comments are (sorry, it has to do with my own purpose above)
+tiny_bottom = A - d / 2
+tiny_height = (tiny_bottom) / A * B
 
-    # Your initial variables are named A, B and d and will have the values
-    # you indicated below.
+big_bottom = A - (d / 2) / 2
+big_height = (big_bottom) / A * B
 
+area = (big_bottom * big_height / 2 - tiny_bottom * tiny_height / 2) * 2
+if d>A :
+    print ("doesn't work")
+print(area)
 
-
-
-
-
-
-
-
-
-
-
-
-    # Print whatever the final area is here! Just the value please, nothing else.
-    print()
-
-
-
-
-if __name__ == '__main__': # please don't change
-    # You can change the below values of A, B, and d to try different combinations
-    A = 10
-    B = 5
-    d = 5
-
-    find_area(A,B,d) # don't change this line or add anything below this
